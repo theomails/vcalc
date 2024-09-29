aa014344044b72607013167af30e45bc359258ad
Theodore Ravindranath <theodore.ravindranath@redflex.com>
2024-09-29 19:06:26 +0530

    First working version - no validations

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,80 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+
+    <div class="boxed calc" style="width:300px;margin:auto">
+      <table width="100%">
+
+        <tr>
+          <td colspan="4">
+            <div class="boxed" style="background-color: lightblue;"> 
+              &nbsp; {{ display }}
+            </div>
+          </td>
+        </tr>
+
+        <tr>
+          <td v-for="num in [1,2,3]" :key="num" width="25%">
+            <div class="boxed" @click="numClick(num)">
+              {{ num }}
+            </div>
+          </td>
+          <td width="25%">
+            <div class="boxed" @click="opClick('+')">
+              +
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <td v-for="num in [4,5,6]" :key="num" width="25%">
+            <div class="boxed" @click="numClick(num)">
+              {{ num }}
+            </div>
+          </td>
+          <td width="25%">
+            <div class="boxed" @click="opClick('-')">
+              -
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <td v-for="num in [7,8,9]" :key="num" width="25%">
+            <div class="boxed" @click="numClick(num)">
+              {{ num }}
+            </div>
+          </td>
+          <td width="25%">
+            <div class="boxed" @click="opClick('*')">
+              *
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <td >
+            <div class="boxed"  @click="clear">
+              Clear
+            </div>
+          </td>
+          <td>
+            <div class="boxed" @click="numClick(0)">
+              0
+            </div>
+          </td>
+          <td width="25%">
+            <div class="boxed" @click="eqClick">
+              =
+            </div>
+          </td>
+          <td width="25%">
+            <div class="boxed" @click="opClick('/')">
+              /
+            </div>
+          </td>
+        </tr>
+
+      </table>
+    </div>
+
   </div>
 </template>
 
@@ -35,12 +83,71 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data(){
+    return {
+      display: '',
+      val1: '',
+      val2: '',
+      operation: ''
+    };
+  },
+  methods:{
+    numClick(num){
+      if('+-*/'.indexOf(this.display) >= 0){
+        this.operation = this.display;
+        this.display = '';
+      }
+
+      this.display=this.display+num;
+    },
+    opClick(op){
+      this.val1=parseFloat(this.display);
+      this.display=op;
+    },
+    eqClick(){
+      this.val2=parseFloat(this.display);
+      if(this.operation == '+'){
+        this.display = this.val1 + this.val2;
+      }else if(this.operation == '-'){
+        this.display = this.val1 - this.val2;
+      }else if(this.operation == '*'){
+        this.display = this.val1 * this.val2;
+      }else if(this.operation == '/'){
+        this.display = this.val1 / this.val2;
+      }
+    },
+    clear(){
+      this.display= '';
+      this.val1= '';
+      this.val2= '';
+      this.operation= '';
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+* { box-sizing: border-box }
+
+.calc{
+  font-family: 'Courier New', Courier, monospace;
+}
+.boxed{
+  border: 1px solid black;
+  display: inline-block;
+  padding: 10px 5px;
+  width: 100%;
+  background-color: #d3b88c;
+  color: black;
+  cursor: pointer;
+}
+
+.boxed:hover{
+  background-color: #e4cea9;
+}
+
 h3 {
   margin: 40px 0 0;
 }
